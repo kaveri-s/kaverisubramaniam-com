4f144f78106dfd466d66a1a99523fb6b187faaa4
kaveri-s <kaveri.subra@gmail.com>
2023-03-20 00:39:14 -0700

    Fix GraphQL twitterUsername error

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Kaveri Subramaniam is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://kaverisubramaniam.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitter: '',
+    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
