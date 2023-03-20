204b6e93f7c90c52ef6a64905cb7cc843171702b
kaveri-s <kaveri.subra@gmail.com>
2023-03-20 00:37:32 -0700

    Fix twitter graphql error

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
       'Kaveri Subramaniam is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://kaverisubramaniam.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
+    twitter: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
