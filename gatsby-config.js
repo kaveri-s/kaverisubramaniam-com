b1a5228c1c77a21557c4398d14c6bce26746d194
kaveri-s <kaveri.subra@gmail.com>
2023-07-18 12:31:24 -0700

    Update Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Kaveri Subramaniam',
     description:
-      'Kaveri Subramaniam is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Kaveri Subramaniam is a software engineer who specializes in designing, developing, and deploying distributed systems on the cloud.',
     siteUrl: 'https://kaverisubramaniam.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
